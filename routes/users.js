a495646a03e762cdaa4d2b7b155724f7afb7722a
Sam Hollyer <rsamhollyer@gmail.com>
2021-08-04 03:22:29 -0500

    updated passport

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,29 @@
 const mongoose = require('mongoose');
-const router = require('express').Router();   
+const router = require('express').Router();
+
 const User = mongoose.model('User');
 const passport = require('passport');
 const utils = require('../lib/utils');
 
 // TODO
-router.get('/protected', (req, res, next) => {
+router.get('/protected', (req, res, next) => {});
+
+// TODO
+router.post('/login', (req, res, next) => {});
+
+router.post('/register', (req, res, next) => {
+  const { salt, hash } = utils.genPassword(req.body.password);
+
+  const newUser = new User({
+    username: req.body.username,
+    hash,
+    salt,
+  });
+
+  newUser
+    .save()
+    .then(user => res.json({ success: true, user }))
+    .catch(err => next(err));
 });
-
-// TODO
-router.post('/login', function(req, res, next){});
-
-// TODO
-router.post('/register', function(req, res, next){});
 
 module.exports = router;
